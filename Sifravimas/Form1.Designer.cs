d2aac8873f70fbf6f6f0150e2c133988146b35b5
Lukas Gecius <lukas.gecius@stud.viko.lt>
2021-04-02 17:38:50 +0300

    Vinegre-cypher
    Collage score - 10;

---
+++ Sifravimas/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Sifravimas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.boxTekstas = new System.Windows.Forms.TextBox();
+            this.boxRaktas = new System.Windows.Forms.TextBox();
+            this.boxSifras = new System.Windows.Forms.TextBox();
+            this.boxSifruotas = new System.Windows.Forms.TextBox();
+            this.boxRaktas2 = new System.Windows.Forms.TextBox();
+            this.boxAtsifruotas = new System.Windows.Forms.TextBox();
+            this.Raktas = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.ButSifruot = new System.Windows.Forms.Button();
+            this.butAtsifruot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Tekstas";
+            // 
+            // boxTekstas
+            // 
+            this.boxTekstas.Location = new System.Drawing.Point(104, 27);
+            this.boxTekstas.Name = "boxTekstas";
+            this.boxTekstas.Size = new System.Drawing.Size(100, 20);
+            this.boxTekstas.TabIndex = 0;
+            // 
+            // boxRaktas
+            // 
+            this.boxRaktas.Location = new System.Drawing.Point(104, 57);
+            this.boxRaktas.Name = "boxRaktas";
+            this.boxRaktas.Size = new System.Drawing.Size(100, 20);
+            this.boxRaktas.TabIndex = 1;
+            // 
+            // boxSifras
+            // 
+            this.boxSifras.Location = new System.Drawing.Point(104, 88);
+            this.boxSifras.Name = "boxSifras";
+            this.boxSifras.Size = new System.Drawing.Size(100, 20);
+            this.boxSifras.TabIndex = 2;
+            // 
+            // boxSifruotas
+            // 
+            this.boxSifruotas.Location = new System.Drawing.Point(350, 27);
+            this.boxSifruotas.Name = "boxSifruotas";
+            this.boxSifruotas.Size = new System.Drawing.Size(100, 20);
+            this.boxSifruotas.TabIndex = 3;
+            // 
+            // boxRaktas2
+            // 
+            this.boxRaktas2.Location = new System.Drawing.Point(350, 57);
+            this.boxRaktas2.Name = "boxRaktas2";
+            this.boxRaktas2.Size = new System.Drawing.Size(100, 20);
+            this.boxRaktas2.TabIndex = 4;
+            // 
+            // boxAtsifruotas
+            // 
+            this.boxAtsifruotas.Location = new System.Drawing.Point(350, 88);
+            this.boxAtsifruotas.Name = "boxAtsifruotas";
+            this.boxAtsifruotas.Size = new System.Drawing.Size(100, 20);
+            this.boxAtsifruotas.TabIndex = 5;
+            // 
+            // Raktas
+            // 
+            this.Raktas.AutoSize = true;
+            this.Raktas.Location = new System.Drawing.Point(12, 60);
+            this.Raktas.Name = "Raktas";
+            this.Raktas.Size = new System.Drawing.Size(41, 13);
+            this.Raktas.TabIndex = 6;
+            this.Raktas.Text = "Raktas";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 91);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Sifruotas tekstas";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(225, 30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(85, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Sifruotas tekstas";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(225, 60);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "Raktas";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(225, 91);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(93, 13);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "Atsifruotas tekstas";
+            // 
+            // ButSifruot
+            // 
+            this.ButSifruot.Location = new System.Drawing.Point(119, 114);
+            this.ButSifruot.Name = "ButSifruot";
+            this.ButSifruot.Size = new System.Drawing.Size(75, 23);
+            this.ButSifruot.TabIndex = 11;
+            this.ButSifruot.Text = "Sifruoti";
+            this.ButSifruot.UseVisualStyleBackColor = true;
+            this.ButSifruot.Click += new System.EventHandler(this.ButSifruot_Click);
+            // 
+            // butAtsifruot
+            // 
+            this.butAtsifruot.Location = new System.Drawing.Point(361, 114);
+            this.butAtsifruot.Name = "butAtsifruot";
+            this.butAtsifruot.Size = new System.Drawing.Size(75, 23);
+            this.butAtsifruot.TabIndex = 12;
+            this.butAtsifruot.Text = "Atsifruoti";
+            this.butAtsifruot.UseVisualStyleBackColor = true;
+            this.butAtsifruot.Click += new System.EventHandler(this.butAtsifruot_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(546, 384);
+            this.Controls.Add(this.butAtsifruot);
+            this.Controls.Add(this.ButSifruot);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Raktas);
+            this.Controls.Add(this.boxAtsifruotas);
+            this.Controls.Add(this.boxRaktas2);
+            this.Controls.Add(this.boxSifruotas);
+            this.Controls.Add(this.boxSifras);
+            this.Controls.Add(this.boxRaktas);
+            this.Controls.Add(this.boxTekstas);
+            this.Controls.Add(this.label9);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boxNesifruotasTekstas;
+        private System.Windows.Forms.TextBox boxSifravimoRaktas;
+        private System.Windows.Forms.TextBox boxUzsifruotasRaktas;
+        private System.Windows.Forms.TextBox BoxUzsifruotasTekstas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button butSifruoti;
+        private System.Windows.Forms.TextBox boxSifruotasTekstas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox BoxAtsifruotasTekstas;
+        private System.Windows.Forms.Button butAtsifruoti;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox boxTekstas;
+        private System.Windows.Forms.TextBox boxRaktas;
+        private System.Windows.Forms.TextBox boxSifras;
+        private System.Windows.Forms.TextBox boxSifruotas;
+        private System.Windows.Forms.TextBox boxRaktas2;
+        private System.Windows.Forms.TextBox boxAtsifruotas;
+        private System.Windows.Forms.Label Raktas;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button ButSifruot;
+        private System.Windows.Forms.Button butAtsifruot;
+    }
+}
+
---
+++ Sifravimas/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sifravimas
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		private void ButSifruot_Click(object sender, EventArgs e)
+		{
+
+			
+			char[] tekstas = boxTekstas.Text.ToCharArray();
+			int[] toDec = new int[tekstas.Length];
+			for (int i = 0; i < tekstas.Length; i++) { toDec[i] = (int)tekstas[i]; }
+
+		
+			char[] raktas = boxRaktas.Text.ToCharArray();
+
+			// istempimas
+
+			char[] raktasExtended = new char[tekstas.Length];
+			int k = 0;
+			for (int i = 0; i < raktasExtended.Length; i++)
+            {
+				if (k < raktas.Length) {  raktasExtended[i] = raktas[k]; k++;  } else { k = 0; i--; }
+				
+            }
+
+			// Convertas į dec
+
+			int[] toDecRaktas = new int[raktasExtended.Length];
+			for (int i = 0; i < raktasExtended.Length; i++) { toDecRaktas[i] = (int)raktasExtended[i]; }
+
+			// Sifravimas
+
+			int asciiLength = 127;
+			//int k = 0; // rakto resetui
+			int[] sifruotasMasyvas = new int[tekstas.Length];
+
+			for (int i = 0; i < tekstas.Length; i++)
+			{
+				//if (k < toDecRaktas.Length){ k = 0; } else { k++ }
+				sifruotasMasyvas[i] = (toDec[i] + toDecRaktas[i] + asciiLength) % asciiLength;
+
+			}
+
+			// int > char
+			char[] charMas = new char[sifruotasMasyvas.Length];
+			for (int i = 0; i < sifruotasMasyvas.Length; i++) { charMas[i] = (char)sifruotasMasyvas[i]; }
+
+			boxSifras.Text = string.Join("", charMas);
+
+		}
+
+        private void butAtsifruot_Click(object sender, EventArgs e)
+        {
+			// teksto string > char > int
+			char[] tekstas = boxSifruotas.Text.ToCharArray();
+			int[] toDec = new int[tekstas.Length];
+			for (int i = 0; i < tekstas.Length; i++) { toDec[i] = (int)tekstas[i]; }
+
+
+			char[] raktas = boxRaktas2.Text.ToCharArray();
+
+			// istempimas
+
+			char[] raktasExtended = new char[tekstas.Length];
+			int k = 0;
+			for (int i = 0; i < raktasExtended.Length; i++)
+			{
+				if (k < raktas.Length) { raktasExtended[i] = raktas[k]; k++; } else { k = 0; i--; }
+
+			}
+
+			// Convertas į dec
+
+			int[] toDecRaktas = new int[raktasExtended.Length];
+			for (int i = 0; i < raktasExtended.Length; i++) { toDecRaktas[i] = (int)raktasExtended[i]; }
+
+			// Sifravimas
+
+			int asciiLength = 127;
+			//int k = 0; // rakto resetui
+			int[] sifruotasMasyvas = new int[tekstas.Length];
+
+			for (int i = 0; i < tekstas.Length; i++)
+			{
+				//if (k < toDecRaktas.Length){ k = 0; } else { k++ }
+				sifruotasMasyvas[i] = (toDec[i] - toDecRaktas[i] + asciiLength) % asciiLength;
+
+			}
+
+			// int > char
+			char[] charMas = new char[sifruotasMasyvas.Length];
+			for (int i = 0; i < sifruotasMasyvas.Length; i++) { charMas[i] = (char)sifruotasMasyvas[i]; }
+
+			boxAtsifruotas.Text = string.Join("", charMas);
+
+		}
+	}
+    
+}
+
